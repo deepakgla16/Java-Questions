42d52c384803997ef120cf2a8a7eda6d23f22392
F_17_Deepak <91952229+deepakgla16@users.noreply.github.com>
2022-09-24 19:05:54 +0530

    Question 3   assignment 1

---
+++ Command.java
@@ -0,0 +1,13 @@
+//Question 2. Write the code to print command line arguments as input.
+
+
+import java.util.*;
+
+public class Command{
+
+public static void main(String[] args){
+
+System.out.println(args[2]);
+
+}
+}
