ccb2aaa1cc1a01ab7a9c087c7f43221533fe935b
F_17_Deepak <91952229+deepakgla16@users.noreply.github.com>
2022-09-24 19:08:03 +0530

    Question 6   assignment 1

---
+++ Messageprint.java
@@ -0,0 +1,12 @@
+//Question 1. Write the code to print “hello” message.
+
+import java.util.*;
+
+class Messageprint{
+
+public static void main(String[] args){
+
+System.out.println("hello");
+
+}
+}
