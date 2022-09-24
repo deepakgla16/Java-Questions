a9d99d8d8b484f902c617ca761336692b606bf3c
F_17_Deepak <91952229+deepakgla16@users.noreply.github.com>
2022-09-24 19:04:37 +0530

    Question 1    assignment 1

---
+++ Area_circle.java
@@ -0,0 +1,14 @@
+//Question 4. Write the code to print area of circle using constant float values. 
+
+
+class Area_circle{
+
+public static void main(String[] args){
+
+double rad = 19.5;
+
+
+System.out.println("Area of a circle : "+((rad*rad)*3.14));
+}
+
+}
