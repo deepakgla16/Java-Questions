cf9887ca10b4194d1873ce1b91cec10a00e3e5d7
F_17_Deepak <91952229+deepakgla16@users.noreply.github.com>
2022-09-24 19:06:51 +0530

    Question 4   assignment 1

---
+++ Constantvalue.java
@@ -0,0 +1,18 @@
+// Question 3. Write the program to print sum, multiply, divide and subtraction of two numbers using constant values.
+
+
+class Constantvalue{
+
+
+public static void main(String[] args){
+
+int a = 20;
+int b = 10;
+
+System.out.println("Sum of two numbers is : "+(a+b));
+System.out.println("Multiply of two numbers is : "+(a*b));
+System.out.println("Division of two numbers is : "+(a/b));
+System.out.println("Subtraction of two numbers is : "+(a-b));
+
+}
+}
